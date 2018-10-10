cd4fc77d7db2746b6b4445032585e2f88027e38e
Nguyen <ngokhoaisan1999@gmail.com>
2018-10-10 14:20:31 +0700

    update

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -22,19 +22,19 @@ public class Week3Test {
 		assertEquals(2, Week3.minOfArray(new int[] { 2, 3, 4, 5, 6 }));
 		assertEquals(0, Week3.minOfArray(new int[] { 0, 3, 4, 5, 6 }));
 		assertEquals(-2, Week3.minOfArray(new int[] { -1, -2, 0, 1, 2 }));
-		assertEquals(1, Week3.minOfArray(new int[] { 1,1,1,1,1,1,1,1 }));
+		assertEquals(1, Week3.minOfArray(new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }));
 		assertEquals(0, Week3.minOfArray(new int[] { 0, 9999, 0, 9999 }));
-		assertEquals(0, Week3.minOfArray(new int[] { 0,0,0,0,0 }));
+		assertEquals(0, Week3.minOfArray(new int[] { 0, 0, 0, 0, 0 }));
 
 	}
 
 	// TODO: Viết 5 testcase cho phương thức calculateBMI()
 	@Test
 	public void testBMI() {
-		assertEquals("Thieu can",Week3.calculateBMI(1, 1));
-		assertEquals("Binh thuong",Week3.calculateBMI(60, 1.7));
-		assertEquals("Thua can",Week3.calculateBMI(69, 1.7));
-		assertEquals("Beo phi",Week3.calculateBMI(100, 1.6));
-		assertEquals("Beo phi",Week3.calculateBMI(9999, 9));
+		assertEquals("Thieu can", Week3.calculateBMI(1, 1));
+		assertEquals("Binh thuong", Week3.calculateBMI(60, 1.7));
+		assertEquals("Thua can", Week3.calculateBMI(69, 1.7));
+		assertEquals("Beo phi", Week3.calculateBMI(100, 1.6));
+		assertEquals("Beo phi", Week3.calculateBMI(9999, 9));
 	}
 }
