68cc4bbd4bb8f7c5dde03bb1393d3318c0bf5e2a
damdauvaotran <ngokhoaisan1999@gmail.com>
2018-11-07 00:01:18 +0700

    chiu

--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -1,4 +1,23 @@
 package week9;
 
+import java.io.*;
+
 public class Utils {
+    public static String readContentFromFile(String path) {
+        StringBuilder result = new StringBuilder("");
+        try {
+            File file = new File(path);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line =reader.readLine())!= null){
+                result.append(line);
+                result.append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
 }
