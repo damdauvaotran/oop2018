7363678230a6b054aef26e35e13596fcdeae59ae
Nguyen <ngokhoaisan1999@gmail.com>
2018-11-11 21:07:59 +0700

    week 9

--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -9,16 +9,48 @@ public class Utils {
             File file = new File(path);
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String line;
-            while ((line =reader.readLine())!= null){
+            while ((line = reader.readLine()) != null) {
                 result.append(line);
                 result.append("\n");
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result.toString();
-
     }
+
+    public static void writeContentToFile(String path) {
+        File file = new File(path);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write("fkajsdhfkhasjdfhjkasdhfjka");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeContentToFileAppend(String path) {
+        File file = new File(path);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, true)); // open in append mode
+            writer.write("fkajsdhfkhasjdfhjkasdhfjka");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static File findFileNameByName(String folderPath, String fileName) {
+        File file = new File(folderPath + File.separator + fileName);
+        if (file.exists() && file.isFile()) {
+            System.out.println(file.getName());
+            return file;
+        }
+
+        return null;
+    }
+
+
 }
